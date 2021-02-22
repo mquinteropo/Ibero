aac1d64ebe331f41daa6206b0723570592d33cac
Miguel Quintero <mquinteropo@gmail.com>
2021-02-22 16:11:03 -0500

    Update URLs
    Update URLs

--- lib/data/apiService.dart
+++ lib/data/apiService.dart
@@ -7,19 +7,19 @@ class ApiService {
     switch (optionUrl) {
       //All articles about Tesla from the last month, sorted by recent first
       case 1:
-        return 'https://newsapi.org/v2/everything?q=tesla&from=2021-01-18&sortBy=publishedAt&apiKey=64c32a49e1df4baa8f23a70a562e3c0c';
+        return 'https://newsapi.org/v2/everything?q=apple&from=2021-02-21&to=2021-02-21&sortBy=popularity&apiKey=f5411eeab72d4c92adfc9957d6d40537';
       //Top business headlines in the US right now
       case 2:
-        return 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=64c32a49e1df4baa8f23a70a562e3c0c';
+        return 'https://newsapi.org/v2/everything?q=tesla&from=2021-01-22&sortBy=publishedAt&apiKey=f5411eeab72d4c92adfc9957d6d40537';
       //All articles mentioning Apple from yesterday, sorted by popular publishers first
       case 3:
-        return 'https://newsapi.org/v2/everything?q=apple&from=2021-02-10&to=2021-02-10&sortBy=popularity&apiKey=64c32a49e1df4baa8f23a70a562e3c0c';
+        return 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f5411eeab72d4c92adfc9957d6d40537';
       //Top headlines from TechCrunch right now
       case 4:
-        return 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=64c32a49e1df4baa8f23a70a562e3c0c';
+        return 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=f5411eeab72d4c92adfc9957d6d40537';
       //All articles published by the Wall Street Journal in the last 6 months, sorted by recent first
       case 5:
-        return 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=64c32a49e1df4baa8f23a70a562e3c0c';
+        return 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=f5411eeab72d4c92adfc9957d6d40537';
     }
   }
 
